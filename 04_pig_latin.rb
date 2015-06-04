1fde3a796a8ae5975c2a8ad1cff752a69211784d
Tarun <tnt.tarun@gmail.com>
2015-06-03 21:35:49 -0400

    Update 04_pig_latin.rb

--- 04_pig_latin.rb
+++ 04_pig_latin.rb
@@ -1,31 +1,14 @@
 def translate (sentence)
 sry=sentence.split(" ")
-word=sry[0]
-	ary = word.split("")
-	if ary[0]=="a"
-		word.concat("ay")
-	elsif ary[0]=="e"
-		word.concat("ay")
-	elsif ary[0]=="i"
-		word.concat("ay")
-	elsif ary[0]=="o"
-		word.concat("ay")
-	elsif ary[0]=="u"
-		word.concat("ay")
-	else
-		if ary[1]=="a"
-		word.slice(1..-1).concat(ary[0]+"ay")
-		elsif ary[1]=="e"
-		word.slice(1..-1).concat(ary[0]+"ay")
-		elsif ary[1]=="i"
-		word.slice(1..-1).concat(ary[0]+"ay")
-		elsif ary[1]=="o"
-		word.slice(1..-1).concat(ary[0]+"ay")
-		elsif ary[1]=="u"
-		word.slice(1..-1).concat(ary[0]+"ay")
+	word=sry[0]
+		ary = word.split("")
+		if ary[0]=="a" or ary[0]=="e" or ary[0]=="i" or ary[0]=="o" or ary[0]=="u"
+			word.concat("ay")
 		else
-		#vow=word.index{|x|x=="a"}.to_i
-		word.slice(2..-1).concat(ary[0]+ary[1]+"ay")
+			if ary[1]=="a" or ary[1]=="e" or ary[1]=="i" or ary[1]=="o" or ary[1]=="u"
+			word.slice(1..-1).concat(ary[0]+"ay")
+			else
+			word.slice(2..-1).concat(ary[0]+ary[1]+"ay")
+			end
 		end
-	end
 end
