68c45cf28d5262a698d591b28b2ef1d51f4daf57
Tarun <tnt.tarun@gmail.com>
2015-06-05 22:46:45 -0400

    Update 04_pig_latin.rb

--- 04_pig_latin.rb
+++ 04_pig_latin.rb
@@ -1,14 +1,30 @@
 def translate (sentence)
-sry=sentence.split(" ")
-		word=sry[0]
+vowels = ["a", "e", "i", "o", "u"]
+words=sentence.split(" ")
+result = []
+	words.each_with_index do |word, i| 	
+    	translation = ''				
+		word=words[i]
 		ary = word.split("")
-		if ary[0]=="a" or ary[0]=="e" or ary[0]=="i" or ary[0]=="o" or ary[0]=="u"
-			word.concat("ay")
-		else
-			if ary[1]=="a" or ary[1]=="e" or ary[1]=="i" or ary[1]=="o" or ary[1]=="u"
-			word.slice(1..-1).concat(ary[0]+"ay")
-			else
-			word.slice(2..-1).concat(ary[0]+ary[1]+"ay")
-			end
+		if vowels.include? ary[0]
+			translation=word.concat("ay")
+			result << translation
+		elsif "qu" == ary[0]+ary[1]
+			translation=word.slice(2..-1).concat(ary[0]+ary[1]+"ay")
+			result << translation	
+		elsif "qu" == ary[1]+ary[2]
+			translation=word.slice(3..-1).concat(ary[0]+ary[1]+ary[2]+"ay")
+			result << translation
+		elsif vowels.include? ary[1]
+			translation=word.slice(1..-1).concat(ary[0]+"ay")
+			result << translation
+		elsif vowels.include? ary[2]
+			translation=word.slice(2..-1).concat(ary[0]+ary[1]+"ay")
+			result << translation
+		elsif vowels.include? ary[3]
+			translation=word.slice(3..-1).concat(ary[0]+ary[1]+ary[2]+"ay")
+			result << translation
 		end
+	end
+	return result.join(" ")
 end
